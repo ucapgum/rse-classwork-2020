751dc4cd5672a6cd12bfa6cee3568cb15a706140
ucapgum <ucapgum@ucl.ac.uk>
2020-11-06 00:31:04 +0000

    cleanest solution

--- week05-testing/test_times.py
+++ week05-testing/test_times.py
@@ -1,20 +1,15 @@
 import pytest
+import yaml
 from times import compute_overlap_time, time_range
 
-inputs = []
-inputs.append([time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00"), time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60), \
-     [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]])
-inputs.append([time_range("2010-01-12 10:00:00", "2010-01-12 11:00:00"), time_range("2010-01-12 12:00:00", "2010-01-12 13:00:00"), []])
-inputs.append([time_range("2010-01-12 10:00:00", "2010-01-12 11:00:00"), time_range("2010-01-12 12:00:00", "2010-01-12 13:00:00"), []])
-inputs.append([time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00", 2, 60), time_range("2010-01-12 11:00:00", "2010-01-12 13:00:00", 2, 60), \
-     [("2010-01-12 11:00:30", "2010-01-12 11:59:30")]])
-inputs.append([time_range("2010-01-12 10:00:00", "2010-01-12 11:00:00"), time_range("2010-01-12 11:00:00", "2010-01-12 13:00:00"), []])
+with open('week05-testing/fixture.yaml') as f:
+    inputs = yaml.load(f, Loader=yaml.FullLoader)
 
-@pytest.mark.parametrize("interval1, interval2, expected", inputs)
+@pytest.mark.parametrize("interval1, interval2, expected", [x.values() for x in inputs.values()], ids=inputs.keys())
 def test_input(interval1, interval2, expected):
-    result = compute_overlap_time(interval1, interval2)
-    assert result == expected
+    result = compute_overlap_time(eval(interval1), eval(interval2))
+    assert result == list(map(eval, expected))
 
 def test_backwards_date():
     with pytest.raises(ValueError):
-        interval = time_range("2010-01-12 11:00:00", "2010-01-12 10:00:00")
+        time_range("2010-01-12 11:00:00", "2010-01-12 10:00:00")
